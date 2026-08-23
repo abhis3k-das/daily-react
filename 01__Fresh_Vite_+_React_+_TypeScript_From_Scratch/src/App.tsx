0d2fd3de619ffe075090fa63f399224eb5e85338
abhis3k-das <abhis3k.das@gmail.com>
2026-08-23 11:04:08 +0530

    added diff env files and created build and previewed it

--- 01__Fresh_Vite_+_React_+_TypeScript_From_Scratch/src/App.tsx
+++ 01__Fresh_Vite_+_React_+_TypeScript_From_Scratch/src/App.tsx
@@ -2,11 +2,12 @@ import './App.css'
 import ProfileCard from './components/ProfileCard';
 
 function App() {
-
+  const server = import.meta.env.VITE_SERVER;
   return <main>
     <h1>Profiles : </h1>
     <ProfileCard name="Test-1" role="Developer" experience={4}/>
     <ProfileCard name="Test-2" role="Product Manager" experience={6}/>
+    <p>Server : {server}</p>
   </main>
 }
 
